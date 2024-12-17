e3b390ded2f24a1b1b405ed56776cca7a3fc2014
Mohanad Abotaleb <mohanado2370@gmail.com>
2024-12-17 18:49:28 +0200

    push next build files
    I lowkey forgot them

---
+++ nextjs-dashboard/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"V6gtZMPMIEgizhdcK85Q9tmOq51eRGfFlHexUfRTODQ=\"\n}"
---
+++ nextjs-dashboard/.next/static/chunks/app_page_tsx_906bf1._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_906bf1._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_906bf1._.js",
+  "chunks": [
+    "static/chunks/[next]_internal_font_google_323c5c._.css",
+    "static/chunks/_a3a68e._.js"
+  ],
+  "source": "dynamic"
+});
